073f6cfe0eae98308bd429e87a3f3ff3ee60cd7b
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2023-08-02 21:40:19 -0700

    functionality of generate complete

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,19 +18,22 @@ const CreatePost = () => {
     if(form.prompt){
       try {
         setgeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+          method: "POST", 
+          headers: { "Content-Type": "application/json", 
           },
           body: JSON.stringify({ prompt: form.prompt })
         })
 
         const data = await response.json();
-        setForm({...form, photo: `data:image/jpeg;base64`})
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (error) {
-        
+        alert(error);
+      } finally {
+        setgeneratingImg(false);
       }
+    } else {
+      alert('Please enter a prompt')
     }
   }
 
